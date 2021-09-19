10f1a3993b9d268efe1145049fc5b1d4ae55c672
Nejc Šneberger <nejc.snebi@gmail.com>
2021-09-19 16:53:11 +0200

    modified server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const URL = process.env.URL || "http://localhost";
+const URL = process.env.URL || "http://192.168.125.28";
 const PORT = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const axios = require("axios");
@@ -8,7 +8,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 server.use(cors({ origin: `${URL}:8080` }));
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "http://192.168.125.28:4000";
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
