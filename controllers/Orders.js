111a8f129c9d91f7b2888a8e67953db971392658
Nejc Šneberger <nejc.snebi@gmail.com>
2021-09-23 00:05:32 +0200

    only one modification to orders controller

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -1,69 +1,74 @@
-const mongoose = require('mongoose');
-const Order = mongoose.model('Order');
+const mongoose = require("mongoose");
+const Order = mongoose.model("Order");
 
 exports.addOrder = (req, res) => {
-    let order = {
-        items: req.body.items
-    }
+	// let order = {
+	//     items: req.body.items
+	// }
 
-    if (!order.items || order.items.length == 0) {
-        return res.status(400).send({
-            message: 'Naročiti morate vsaj en izdelek'
-        });
-    }
+	console.log(req.body);
 
-    let newOrder = new Order({
-        items: order.items
-    });
+	// if (!order.items || order.items.length == 0) {
+	//     return res.status(400).send({
+	//         message: 'Naročiti morate vsaj en izdelek'
+	//     });
+	// }
 
-    newOrder.save(newOrder)
-        .then(() => {
-            res.status(201).send({
-                message: 'Naročilo je bilo oddano.'
-            });
-        })
-        .catch(err => {
-            res.status(400).send({
-                message: err
-            })
-        });
-}
+	// let newOrder = new Order({
+	//     items: order.items
+	// });
+
+	// newOrder.save(newOrder)
+	//     .then(() => {
+	//         res.status(201).send({
+	//             message: 'Naročilo je bilo oddano.'
+	//         });
+	//     })
+	//     .catch(err => {
+	//         res.status(400).send({
+	//             message: err
+	//         })
+	//     });
+	res.send({
+		message: "hehe",
+	});
+};
 
 exports.getOrders = (req, res) => {
-    Order.find()
-        .then(orders => {
-            if (!orders) {
-                return res.status(404).send({
-                    message: 'Naročila niso bila najdena!'
-                });
-            }
-            res.json({
-                orders
-            });
-        })
-        .catch(err => {
-            res.status(400).send({
-                message: err
-            });
-        });
-}
+	Order.find()
+		.then((orders) => {
+			if (!orders) {
+				return res.status(404).send({
+					message: "Naročila niso bila najdena!",
+				});
+			}
+			res.json({
+				orders,
+			});
+		})
+		.catch((err) => {
+			res.status(400).send({
+				message: err,
+			});
+		});
+};
 
 exports.getOrder = (req, res) => {
-    Order.findById(req.params.id)
-    .then(order => {
-        if (!order) {
-            return res.status(404).send({
-                message: 'Naročilo ni bil najdeno!'
-            });
-        }
-        console.log(order);
-        res.json({
-            order
-        });
-    })
-    .catch(err => {
-        res.status(400).send({
-            message: err
-        });
-    });
-}
+	Order.findById(req.params.id)
+		.then((order) => {
+			if (!order) {
+				return res.status(404).send({
+					message: "Naročilo ni bil najdeno!",
+				});
+			}
+			console.log(order);
+			res.json({
+				order,
+			});
+		})
+		.catch((err) => {
+			res.status(400).send({
+				message: err,
+			});
+		});
+};
